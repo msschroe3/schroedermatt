3dc5f7ad129a7562e800d551b21930f7cb9ca8ed
Matt Schroeder <matt.schroeder@objectpartners.com>
2018-08-20 10:16:50 -0500

    fix reference to deleted image

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import Link from 'gatsby-link'
-import pic02 from '../images/pic02.jpg'
 import About from './About';
 import Contact from './Contact';
 import Work from './Work';
