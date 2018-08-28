0fdac6185e544cbd39e2d8890fcf13f5dbe66842
Matt Schroeder <matt.schroeder@objectpartners.com>
2018-08-28 12:23:29 -0500

    added Social to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import Social from './Social'
 
 const Footer = ({ timeout }) => (
     <footer id="footer" style={timeout ? {display: 'none'} : {}}>
+        <Social />
         <p className="copyright">&copy; Matt Schroeder MMXVIII. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
     </footer>
 )
