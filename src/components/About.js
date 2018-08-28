749f72f7122dbda28cd037c8d69a0e6e4122cfe1
Matt Schroeder <matt.schroeder@objectpartners.com>
2018-08-28 12:23:44 -0500

    improved layout and content on About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import my_face from '../images/my_face.png'
+import family from '../images/family.jpg'
 
 const About = ({ active, timeout, onClose }) => (
     <article id="about" className={`${active ? 'active' : ''} ${timeout ? 'timeout' : ''}`} style={{display:'none'}}>
         <h2 className="major">About</h2>
         <span className="avatar">
             <img src={my_face} alt="Matt Schroeder" />
+            <p>A wide variety of professional experience and a Master's Degree in Software Engineering have become the foundation that enables Matt to lead teams to the best solution for every problem.</p>
         </span>
-        <p>5+ years of professional experience and a Master's Degree in Software Engineering have become the foundation that enables Matt to lead teams to the best solution for every problem.</p>
+        
+        <span className="avatar">
+            <p>When offline, Matt enjoys spending time outdoors with his family and friends.</p>
+            <img className="right" src={family} alt="Matt &amp; Daughter" />
+        </span>
         
         <div className="close" onClick={() => {onClose()}} />
     </article>
